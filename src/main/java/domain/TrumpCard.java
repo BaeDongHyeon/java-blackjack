e1cc09fa1e91d0ce447a48ad12c2cc46379423a2
BaeDongHyeon <sasd730@gmail.com>
2023-03-26 21:59:48 +0900

    feat: TrumpCard 클래스 생성

---
+++ src/main/java/domain/TrumpCard.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.List;
+
+public class TrumpCard {
+
+    private final List<Card> cards;
+
+    public TrumpCard(List<Card> cards) {
+        this.cards = cards;
+    }
+}
