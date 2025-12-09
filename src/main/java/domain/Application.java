e7fb4d90788e58e5fb241ce630979b3090e52c63
BaeDongHyeon <sasd730@gmail.com>
2025-12-09 20:27:24 +0900

    init: 버전 업그레이드 및 테스트

--- src/main/java/domain/Application.java
+++ src/main/java/domain/Application.java
@@ -1,7 +1,9 @@
 package domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-
+        String console = Console.readLine();
     }
 }
